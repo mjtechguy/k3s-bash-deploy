1a2a08989162005966402f67fb263d72544f3c7d
Mike 'MJ' Johnson <mikejohnsonit@gmail.com>
2023-05-09 23:53:11 +0000

    update script to create ~/.kube dir

--- single-node/deploy.sh
+++ single-node/deploy.sh
@@ -3,12 +3,10 @@
 # Install k3s
 curl -sfL https://get.k3s.io | sh -
 
-# Add current user to the "docker" group to allow running containers without sudo
-sudo usermod -aG docker $USER
-
 # Wait for k3s to start
 sleep 5
 
 # Get the k3s kubeconfig file
+mkdir ~/.kube
 sudo cat /etc/rancher/k3s/k3s.yaml > ~/.kube/config
 sudo chown $(id -u):$(id -g) ~/.kube/config
